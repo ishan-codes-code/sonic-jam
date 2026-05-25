6e7d9860eaf7e37aee53b8ce10ca3d0c8a9c00f7
Ishan Srivastava <dev.srivastava.ishan@gmail.com>
2026-05-25 13:02:02 +0000

    fix console statment in production

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,11 @@ module.exports = function (api) {
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
         ],
+        env: {
+            production: {
+                plugins: [['transform-remove-console']],
+            },
+        },
         plugins: [
             'react-native-reanimated/plugin',
         ],
